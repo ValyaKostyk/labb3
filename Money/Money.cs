d6dcdf19c54260daf3fdcb1a12a446ab25654458
ValyaKostyk <33714216+ValyaKostyk@users.noreply.github.com>
2019-06-06 11:24:13 +0300

    Add files via upload

---
+++ Money/Money.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class Money
+    {
+        public Money(string currency, double amount = 0)
+        {
+            Currency = currency;
+            Amount = amount;
+        }
+
+        public double Amount { get; set; }
+        public string Currency { get; private set; }
+
+        public void ChangeCurrency(string newCurrency)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
