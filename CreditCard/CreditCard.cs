f0274a4f9c77d7307d3cbf45c31f22c8bb32f373
coffeely <37831580+coffeely@users.noreply.github.com>
2019-06-06 11:10:02 +0300

    Add files via upload

---
+++ CreditCard/CreditCard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    public class CreditCard
+    {
+        
+        public double Balance { get; set; }
+
+        public void Deposit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Widthdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
