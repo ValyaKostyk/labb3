cd2aa779abc0b420d5d82bcb5c7e8a21f9ca521f
coffeely <37831580+coffeely@users.noreply.github.com>
2019-06-06 11:11:15 +0300

    Add files via upload

---
+++ BuisnesCard/BuisnesCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnesCard
+{
+    public class BuisnesCard
+    {
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Widthdraw(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
